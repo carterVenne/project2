922851dc8b01ecfe60d93a96688655a9cd53df37
Carter Venne <you@example.com>
2021-04-01 18:26:40 -0500

    click/add-comment-button

--- public/js/comment.js
+++ public/js/comment.js
@@ -27,4 +27,4 @@ async function commentFormHandler(event) {
       }
   }
   
-  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+  document.querySelector('.add-comment-btn').addEventListener('click', commentFormHandler);
